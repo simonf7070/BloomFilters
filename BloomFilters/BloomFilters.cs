7ea49fedb991fc0747797a203e19512a5027ab97
Simon Felix <simonf7070@gmail.com>
2016-11-25 16:31:03 +0000

    md5 hash calculator

---
+++ BloomFilters/BloomFilters.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace BloomFilters
+{
+    public class BloomFilters
+    {
+        public string CalculateMD5Hash(string input)
+        {
+            var md5 = MD5.Create();
+
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+
+            var hash = md5.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
